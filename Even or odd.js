aebe66adbd651c148f803829d5de30607b321e4e
abhishekkumar956821 <abhishekkumar956821@gmail.com>
2025-07-14 07:22:32 +0530

    first commit

---
+++ Even or odd.js
@@ -0,0 +1,11 @@
+let num = 10;
+if(num % 2 == 0)
+{
+
+    console.log("Even");
+
+}
+else
+{
+    console.log("Odd");
+}
---
+++ add.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b=19;
+// addition 
+let sum = a+b;
+console.log("Sum",sum);
+
+// multiplication 
+let Number = a*b;
+console.log("number",Number);
