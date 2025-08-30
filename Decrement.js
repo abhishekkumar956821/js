240bf8ebff88b501d8e3b159bba5332004e1bfda
abhishekkumar956821 <abhishekkumar956821@gmail.com>
2025-08-30 22:45:11 +0530

    hyy

---
+++ Decrement.js
@@ -0,0 +1,13 @@
+//decrement m "a--" ya "a-1"  both are same
+
+
+let a = 9;
+let b = 2;
+
+console.log("a = ",a,"& b = ",b);
+
+a = a-1;
+b--;
+
+console.log("a = ",a);
+console.log("b = ", b);
---
+++ increment.js
@@ -0,0 +1,13 @@
+//increment m "a++" ya "a+1"  both are same
+
+
+let a = 9;
+let b = 2;
+
+console.log("a = ",a,"& b = ",b);
+
+a++;
+b = b+1;
+
+console.log("a = ",a);
+console.log("b = ", b);
